32507a06ad168ec86b72c6c9586072c6a1d5463c
ykwise <yeswanthdevarasetti@gmail.com>
2024-08-29 19:43:07 +0530

    android application

---
+++ 13sensor/app/src/main/java/com/palaniappan/thillainathan/a13sensor/MainActivity.java
@@ -0,0 +1,41 @@
+package com.palaniappan.thillainathan.a13sensor;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv1;
+    SensorManager sensormanager;
+    List<Sensor> devicesensor;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tv1=(TextView) findViewById(R.id.textview);
+        sensormanager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        devicesensor=sensormanager.getSensorList(Sensor.TYPE_ALL);
+        for(Sensor sensor: devicesensor)
+        {
+            tv1.setText(tv1.getText()+"\n"+sensor.getName());
+        }
+    }
+}
